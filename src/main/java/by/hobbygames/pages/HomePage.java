739fdf9ac72bbe8bfa0229024d251f52e791f29c
Alena Shushkevich <elenashushkevich7@gmail.com>
2024-04-06 11:14:39 +0300

    Initial commit

---
+++ src/main/java/by/hobbygames/pages/HomePage.java
@@ -0,0 +1,13 @@
+package by.hobbygames.pages;
+
+import driver.Driver;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    WebDriver driver;
+
+    public HomePage() {
+        this.driver = Driver.getDriver();
+    }
+
+}
---
+++ src/test/java/ui/BaseTest.java
@@ -0,0 +1,21 @@
+package ui;
+
+import driver.Driver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest {
+    WebDriver driver;
+
+    @BeforeEach
+    public void warmUp() {
+        driver = Driver.getDriver();
+        driver.get("https://hobbygames.by/");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Driver.quitDriver();
+    }
+}
