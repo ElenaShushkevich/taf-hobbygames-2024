f2e0d72a5808ac72f0c65008cdad548fb4014e31
Alena Shushkevich <elenashushkevich7@gmail.com>
2024-04-14 23:11:26 +0300

    add loginApi test3

--- src/test/java/api/LoginTest.java
+++ src/test/java/api/LoginTest.java
@@ -31,4 +31,17 @@ public class LoginTest {
                 post("https://hobbygames.by/?route=account/login/modalAjax").
                 then().statusCode(200).body(equalTo(errorWithoutAnyDate));
     }
+
+    @Test
+    @DisplayName("Status code and message for invalid phone/email")
+    public void testMessageInvalidPhone() {
+        String body = "login=1233323121&password=&scenario=email";
+        String errorWithInvalidPhone = "{\"success\":false,\"errors\":{\"phone\":\"\\u0422\\u0430\\u043a\\u043e\\u0439 \\u0442\\u0435\\u043b\\u0435\\u0444\\u043e\\u043d \\u043d\\u0435 \\u0437\\u0430\\u0440\\u0435\\u0433\\u0438\\u0441\\u0442\\u0440\\u0438\\u0440\\u043e\\u0432\\u0430\\u043d\",\"password\":\"\\u0412\\u0432\\u0435\\u0434\\u0438\\u0442\\u0435 \\u043f\\u0430\\u0440\\u043e\\u043b\\u044c\",\"login\":\"\\u041d\\u0435\\u0432\\u0435\\u0440\\u043d\\u044b\\u0439 \\u0442\\u0435\\u043b\\u0435\\u0444\\u043e\\u043d\\/e-mail\"},\"selected_region_id\":15}";
+        given().
+                contentType("application/x-www-form-urlencoded; charset=UTF-8").
+                body(body).
+                when().
+                post("https://hobbygames.by/?route=account/login/modalAjax").
+                then().statusCode(200).body(equalTo(errorWithInvalidPhone));
+    }
 }
